0748b30d3cda865486f98e01a9987c02acbf829c
mimimi <jjzzzmd@gmail.com>
2019-04-14 16:12:16 +0800

    change hard coded ip to command param, add switch for notification

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import subprocess
 import os
-IP="192.168.31.1"
+import platform
+import sys
+
+SYSTEM_MAC_PLATFORM="Darwin"
+
+IP="192.168.0.1"
 STR_TIME="time="
 STR_MS="ms"
 
@@ -35,37 +40,45 @@ def print_status():
     print("Fail  %s %.2f%% %s"%(failureCounter,failureCounter/total*100,failureRecords[__PRINT_RECENT_COUNT__:]))
     print("--------------------")
 
-proc=subprocess.Popen(["ping", IP],stdout=subprocess.PIPE)
-for line in proc.stdout:
-    lineStr = line.decode('UTF8').strip()
-    if "56 data" in lineStr:
-        continue
-    if "time=" in lineStr:
-        strTimeIndex=lineStr.find(STR_TIME)
-        strMsIndex=lineStr.find(STR_MS)
-        timeStr=lineStr[strTimeIndex+len(STR_TIME):strMsIndex-1]
-        timeF=float(timeStr)
-        if timeF<FAST_UPPER_F:
-            fastCounter+=1
-            fastRecords.append(timeF)
-        elif timeF<=SLOW_THRESHOLD_F:
-            okCounter+=1
-            okRecords.append(timeF)
-        if timeF>SLOW_THRESHOLD_F:
-            slowCounter+=1
-            slowRecords.append(timeF)
-        elif timeF>SUPER_SLOW_THRESHOLD_F:
-            superSlowCounter+=1
-            sslowRecords.append(timeF)
+if __name__ == '__main__':
+    if len(sys.argv)<3:
+        print("Invalid param(s)")
+        exit()
+    IP = sys.argv[1]
+    notificationFlag = int(sys.argv[2])
 
-        #handle notification
-        if timeF>SLOW_THRESHOLD_F:
-            os.system("osascript -e 'display notification \"%sms to %s \" with title \"slow connection detected \"'"%(timeF,IP))
-    else:
-        failureCounter+=1
-        #failure record error msg
-        failureRecords.append(lineStr)
-    total+=1
-    print()
-    print(lineStr)
-    print_status()
+    proc=subprocess.Popen(["ping", IP],stdout=subprocess.PIPE)
+    for line in proc.stdout:
+        lineStr = line.decode('UTF8').strip()
+        if "56 data" in lineStr:
+            continue
+        if "time=" in lineStr:
+            strTimeIndex=lineStr.find(STR_TIME)
+            strMsIndex=lineStr.find(STR_MS)
+            timeStr=lineStr[strTimeIndex+len(STR_TIME):strMsIndex-1]
+            timeF=float(timeStr)
+            if timeF<FAST_UPPER_F:
+                fastCounter+=1
+                fastRecords.append(timeF)
+            elif timeF<=SLOW_THRESHOLD_F:
+                okCounter+=1
+                okRecords.append(timeF)
+            if timeF>SLOW_THRESHOLD_F:
+                slowCounter+=1
+                slowRecords.append(timeF)
+            elif timeF>SUPER_SLOW_THRESHOLD_F:
+                superSlowCounter+=1
+                sslowRecords.append(timeF)
+
+            if platform.system()==SYSTEM_MAC_PLATFORM and notificationFlag:
+                #handle notification
+                if timeF>SLOW_THRESHOLD_F:
+                    os.system("osascript -e 'display notification \"%sms to %s \" with title \"slow connection detected \"'"%(timeF,IP))
+        else:
+            failureCounter+=1
+            #failure record error msg
+            failureRecords.append(lineStr)
+        total+=1
+        print()
+        print(lineStr)
+        print_status()
